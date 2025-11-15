4a29944d58ff7acf99f7caf4ab4c7fdc964b9181
rudraksh-rankawat <133856158+rudraksh-rankawat@users.noreply.github.com>
2025-11-15 11:52:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+
+
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
